1eefaefba188544c9d77aee8c314e1c71dff1f69
ILS - Industrielogistik <marcel.jaworski@ils-gmbh.net>
2024-01-18 17:34:48 +0100

    CRUD done

--- handlers.go/handler.go
+++ handlers.go/handler.go
@@ -2,10 +2,16 @@ package handlers
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
 
+	"go-postgres_api/models"
+
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
@@ -49,7 +55,94 @@ func CreateConnection() *sql.DB {
 		panic(err)
 	}
 
-	fmt.Println("successfully conected to postgres")
+	fmt.Println("Successfully connected to postgres")
 
 	return db
+}
+
+func CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product models.Product
+
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		log.Fatal("Unabel to decode the request body. %v", err)
+	}
+
+	insertID := insertProduct(product)
+
+	res := response{
+		ID:      insertID,
+		Message: "product created succesfully",
+	}
+
+	json.NewEncoder(w).Encode(res)
+}
+func GetProduct(w http.ResponseWriter, r *http.Request) {
+
+	params := mux.Vars(r) // Get acces to the parameters using the mux package
+
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		log.Fatalf("unable to convert the string into int. %v", err)
+	}
+
+	product, err := getProduct(int64(id))
+	if err != nil {
+		log.Fatalf("unable to get stock. %v", err)
+	}
+
+	json.NewEncoder(w).Encode(product)
+}
+func GetAllProducts(w http.ResponseWriter, r *http.Request) {
+	products, err := GetAllProducts()
+	if err != nil {
+		log.Fatalf("unable to get all the products %v", err)
+	}
+
+	json.NewEncoder(w).Encode(products)
+}
+func UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		log.Fatalf("unable to convert the string into int %v", err)
+	}
+
+	var product models.Product
+
+	err = json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		log.Fatalf("unable to decode the request body. %v", err)
+	}
+
+	updatedRows := updateProduct(int64(id), product)
+
+	msg := fmt.Sprintf("Product updated successfully. Total rows/records affected %v", updatedRows)
+
+	res := response{
+		ID:      int64(id),
+		Message: msg,
+	}
+
+	json.NewEncoder(w).Encode(res)
+
+}
+func DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.ParseInt(params["id"])
+	if err != nil {
+		log.Fatalf("unable to convert string to int. %v", err)
+	}
+
+	deletedRows := deleteProduct(int64(id))
+
+	msg := fmt.Sprintf("Product deleted suscessfully. Total rows/records %v", deletedRows)
+
+	res := response{
+		ID:      int64(id),
+		Message: msg,
+	}
+
+	json.NewEncoder(w).Encode(res)
 }
