407d62a233b4f722040fb7422fd5dc5587f19dfe
ILS - Industrielogistik <marcel.jaworski@ils-gmbh.net>
2024-01-19 10:33:38 +0100

    models struct sintax fixed

--- models/models.go
+++ models/models.go
@@ -1,8 +1,8 @@
 package models
 
 type Product struct {
-	ProductID int64  `json:productid`
-	Name      string `json:name`
-	Price     int64  `json:price`
-	Company   string `json:company`
+	ProductID int64  `json:"productid"`
+	Name      string `json:"name"`
+	Price     int64  `json:"price"`
+	Company   string `json:"company"`
 }
