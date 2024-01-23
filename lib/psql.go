1149ec2f4029aa1e83491ca701ae40876773f2b9
ILS - Industrielogistik <marcel.jaworski@ils-gmbh.net>
2024-01-23 12:33:10 +0100

    lib.CreateConnection

---
+++ lib/psql.go
@@ -0,0 +1,49 @@
+package lib
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/cenkalti/backoff"
+	"github.com/joho/godotenv"
+)
+
+func CreateConnection() (*sql.DB, error) {
+	var (
+		db  *sql.DB
+		err error
+	)
+	err = godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Erro loading .env file")
+	}
+
+	host := os.Getenv("PG_HOST")
+	port := os.Getenv("PG_PORT")
+	user := os.Getenv("PG_USER")
+	password := os.Getenv("PG_PASSWORD")
+	dbname := os.Getenv("PG_DBNAME")
+
+	// Postgres connection string
+	psqlConnString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host,
+		port,
+		user,
+		password,
+		dbname,
+	)
+
+	openDB := func() error {
+		db, err = sql.Open("postgres", psqlConnString)
+		return err
+	}
+
+	err = backoff.Retry(openDB, backoff.NewExponentialBackOff())
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
